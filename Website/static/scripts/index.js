a65d9b69e709f3d7b35815ef271c0506600fc7e8
Mahmoud <mahmoudmohamedk8200949@gmail.com>
2024-01-29 20:23:49 +0200

    a

---
+++ Website/static/scripts/index.js
@@ -0,0 +1,32 @@
+
+const navigators = document.querySelectorAll('.nav__item');
+const underline = document.querySelector('.nav__item--horizontal_line');
+
+
+navigators.forEach((navItem)=>{
+    // mouseover
+    const underline = navItem.childNodes[3];
+    underline.style.width = `${navItem.offsetWidth}px`;
+    navItem.addEventListener('mouseover',()=>{
+        underline.classList.add('nav__item--horizontal_line--mouseover')
+    });
+    navItem.addEventListener('mouseout',()=>{
+        underline.classList.remove('nav__item--horizontal_line--mouseover')
+    });
+    
+    // mouseClick
+    navItem.addEventListener('click',()=>{
+        navigators.forEach((anotherNavItem)=>{
+            if(anotherNavItem != navItem){
+                const underline = anotherNavItem.childNodes[3];
+                underline.classList.remove('nav__item--horizontal_line--clicked')
+            }
+        });
+        const underline = navItem.childNodes[3];
+        underline.style.width = `${navItem.offsetWidth}px`;
+        underline.classList.add('nav__item--horizontal_line--clicked')
+        underline.classList.remove('nav__item--horizontal_line--mouseover')
+        
+    });
+    
+});
