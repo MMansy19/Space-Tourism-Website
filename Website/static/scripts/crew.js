3729c157e253ae121965b8a13604af5cc11932d1
Mahmoud <mahmoudmohamedk8200949@gmail.com>
2024-01-30 01:21:06 +0200

    a

---
+++ Website/static/scripts/crew.js
@@ -0,0 +1,49 @@
+/* eslint-disable max-len */
+/* eslint-disable linebreak-style */
+// =====================================================================================================>
+//  <====================================== CREW  CLICK & CHANGING CREWS ===============================>
+// =====================================================================================================>
+const name = document.querySelector('.crew__description--name');
+const job = document.querySelector('.crew__description--job');
+const details = document.querySelector('.crew__description--details');
+const image = document.querySelector('.crew__image');
+const circles = document.querySelectorAll('.circle');
+const changeCrew = (index) => {
+  if (index === 0) {
+    name.innerText = 'Douglas Hurley';
+    job.innerText = 'Commander';
+    details.innerText = 'Douglas Gerald Hurley is an American engineer, former Marine Corps pilot and former NASA astronaut. He launched into space for the third time as commander of Crew Dragon Demo-2.';
+    image.src = '../static/assets/crew/image-douglas-hurley-2.png';
+  } else if (index === 1) {
+    name.innerText = 'Mark Shuttleworth';
+    job.innerText = 'Mission Specialist';
+    details.innerText = 'Mark Richard Shuttleworth is the founder and CEO of Canonical, the company behind the Linux-based Ubuntu operating system. Shuttleworth became the first South African to travel to space as a space tourist.';
+    image.src = '../static/assets/crew/image-mark-shuttleworth.webp';
+  } else if (index === 2) {
+    name.innerText = 'Victor Glover';
+    job.innerText = 'Pilot';
+    details.innerText = "Commander Glover, a U.S. Navy pilot of the F/A-18, was part of the SpaceX Crew Dragon's first operational flight to the International Space Station. He served as a station systems flight engineer during Expedition 64.";
+    image.src = '../static/assets/crew/image-victor-glover.webp';
+  } else if (index === 3) {
+    name.innerText = 'Mahmoud Mansy';
+    job.innerText = 'Full Stack Developer';
+    details.innerText = 'Experienced Full Stack Developer in ReactJS, JavaScript, and Biomedical Engineering. Junior software engineer proficient in Python, Flask, PostgreSQL. Results-driven, strategic thinker, ready to contribute innovatively to projects.';
+    image.src = '../static/assets/crew/mypic.png';
+  } else if (index === 4) {
+    name.innerText = 'Anousheh Ansari';
+    job.innerText = 'Flight Engineer';
+    details.innerText = 'Anousheh Ansari is an Iranian American engineer and co-founder of Prodea Systems. Ansari was the fourth self-funded space tourist, the first self-funded woman to fly to the ISS, and the first Iranian in space.';
+    image.src = '../static/assets/crew/image-douglas-hurley.webp';
+  }
+};
+circles.forEach((circle, index) => {
+  circle.addEventListener('click', () => {
+    circles.forEach((anotherCircle) => {
+      if (anotherCircle !== circle) {
+        anotherCircle.classList.remove('circle--clicked');
+      }
+    });
+    circle.classList.add('circle--clicked');
+    changeCrew(index);
+  });
+});
--- Website/static/scripts/index.js
+++ Website/static/scripts/index.js
@@ -1,3 +1,5 @@
+/* eslint-disable max-len */
+/* eslint-disable linebreak-style */
 // ======================================================================================================>
 //  <================================== HEADER (NAVIGATION) HOVER & CLICK================================>
 // ======================================================================================================>
@@ -17,18 +19,12 @@ navigators.forEach((navItem) => {
   // mouseClick
   navItem.addEventListener('click', () => {
     navigators.forEach((anotherNavItem) => {
-      if (anotherNavItem != navItem) {
-        const underline = anotherNavItem.childNodes[3];
+      if (anotherNavItem !== navItem) {
         underline.classList.remove('nav__item--horizontal_line--clicked');
       }
     });
-    const underline = navItem.childNodes[3];
     underline.style.width = `${navItem.offsetWidth}px`;
     underline.classList.add('nav__item--horizontal_line--clicked');
     underline.classList.remove('nav__item--horizontal_line--mouseover');
   });
 });
-
-// =====================================================================================================>
-//  <====================================== CREW  CLICK & CHANGING CREWS ===============================>
-// =====================================================================================================>
