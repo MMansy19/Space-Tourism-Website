e58ed0965744d8b9fad7af029061afbdc11e8657
Mahmoud <mahmoudmohamedk8200949@gmail.com>
2024-01-31 08:11:11 +0200

    added

--- static/scripts/header.js
+++ static/scripts/header.js
@@ -29,13 +29,15 @@ navigators.forEach((navItem) => {
   });
 });
 // ======================================================================================================>
-//  <================================== HAMBURGER CLICK ================================>
+//  <============================================ HAMBURGER CLICK =======================================>
 // ======================================================================================================>
 const hamburger = document.querySelector('.box-bar');
 const closeBar = document.querySelector('.close-bar');
 const nav = document.querySelector('.nav');
 const home = document.querySelector('.nav__home');
 const destination = document.querySelector('.nav__destination');
+const crew = document.querySelector('.nav__crew');
+const technology = document.querySelector('.nav__technology');
 const mediaQuery = window.matchMedia('(max-width: 800)');
 
 hamburger.addEventListener('click', () => {
@@ -66,10 +68,11 @@ closeBar.addEventListener('click', () => {
   }, 1000); // 1000 milliseconds = 1 second, adjust as needed
 });
 
+// ======================================================================================================>
+//  <===================================== RESPONSIVE FOR MOBILE ========================================>
+// ======================================================================================================>
 if (mediaQuery.matches) {
-// ======================================================================================================>
-//  <=============================================== HOME CLICK =========================================>
-// ======================================================================================================>
+//  <=============================================== HOME CLICK =========================================
   home.addEventListener('click', () => {
     nav.style.transition = 'transform 1s ease';
     nav.style.transform = 'translateX(110%)';
@@ -83,10 +86,36 @@ if (mediaQuery.matches) {
       hamburger.style.display = 'block';
     }, 1000); // 1000 milliseconds = 1 second, adjust as needed
   });
-  // ======================================================================================================>
   //  <========================================= DESTINATION CLICK ========================================>
-  // ======================================================================================================>
   destination.addEventListener('click', () => {
+    nav.style.transition = 'transform 1s ease';
+    nav.style.transform = 'translateX(110%)';
+    closeBar.style.transition = 'opacity 1s ease';
+    closeBar.style.opacity = 0;
+
+    // Adding a delay to ensure the transition completes before hiding elements
+    setTimeout(() => {
+      nav.style.display = 'none';
+      closeBar.style.display = 'none';
+      hamburger.style.display = 'block';
+    }, 1000); // 1000 milliseconds = 1 second, adjust as needed
+  });
+  //  <============================================= CREW CLICK ===========================================>
+  crew.addEventListener('click', () => {
+    nav.style.transition = 'transform 1s ease';
+    nav.style.transform = 'translateX(110%)';
+    closeBar.style.transition = 'opacity 1s ease';
+    closeBar.style.opacity = 0;
+
+    // Adding a delay to ensure the transition completes before hiding elements
+    setTimeout(() => {
+      nav.style.display = 'none';
+      closeBar.style.display = 'none';
+      hamburger.style.display = 'block';
+    }, 1000); // 1000 milliseconds = 1 second, adjust as needed
+  });
+  //  <========================================== TECHNOLOGY CLICK ========================================>
+  technology.addEventListener('click', () => {
     nav.style.transition = 'transform 1s ease';
     nav.style.transform = 'translateX(110%)';
     closeBar.style.transition = 'opacity 1s ease';
