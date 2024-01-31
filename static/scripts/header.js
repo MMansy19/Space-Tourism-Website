f716602139db1dac7082a7f5bf66b1b49be12b17
Mahmoud <mahmoudmohamedk8200949@gmail.com>
2024-01-31 02:08:07 +0200

    done

--- static/scripts/header.js
+++ static/scripts/header.js
@@ -35,6 +35,8 @@ const hamburger = document.querySelector('.box-bar');
 const closeBar = document.querySelector('.close-bar');
 const nav = document.querySelector('.nav');
 const home = document.querySelector('.nav__home');
+const destination = document.querySelector('.nav__destination');
+const mediaQuery = window.matchMedia('(max-width: 800)');
 
 hamburger.addEventListener('click', () => {
   closeBar.style.display = 'block';
@@ -63,16 +65,38 @@ closeBar.addEventListener('click', () => {
     hamburger.style.display = 'block';
   }, 1000); // 1000 milliseconds = 1 second, adjust as needed
 });
-home.addEventListener('click', () => {
-  nav.style.transition = 'transform 1s ease';
-  nav.style.transform = 'translateX(110%)';
-  closeBar.style.transition = 'opacity 1s ease';
-  closeBar.style.opacity = 0;
 
-  // Adding a delay to ensure the transition completes before hiding elements
-  setTimeout(() => {
-    nav.style.display = 'none';
-    closeBar.style.display = 'none';
-    hamburger.style.display = 'block';
-  }, 1000); // 1000 milliseconds = 1 second, adjust as needed
-});
+if (mediaQuery.matches) {
+// ======================================================================================================>
+//  <=============================================== HOME CLICK =========================================>
+// ======================================================================================================>
+  home.addEventListener('click', () => {
+    nav.style.transition = 'transform 1s ease';
+    nav.style.transform = 'translateX(110%)';
+    closeBar.style.transition = 'opacity 1s ease';
+    closeBar.style.opacity = 0;
+
+    // Adding a delay to ensure the transition completes before hiding elements
+    setTimeout(() => {
+      nav.style.display = 'none';
+      closeBar.style.display = 'none';
+      hamburger.style.display = 'block';
+    }, 1000); // 1000 milliseconds = 1 second, adjust as needed
+  });
+  // ======================================================================================================>
+  //  <========================================= DESTINATION CLICK ========================================>
+  // ======================================================================================================>
+  destination.addEventListener('click', () => {
+    nav.style.transition = 'transform 1s ease';
+    nav.style.transform = 'translateX(110%)';
+    closeBar.style.transition = 'opacity 1s ease';
+    closeBar.style.opacity = 0;
+
+    // Adding a delay to ensure the transition completes before hiding elements
+    setTimeout(() => {
+      nav.style.display = 'none';
+      closeBar.style.display = 'none';
+      hamburger.style.display = 'block';
+    }, 1000); // 1000 milliseconds = 1 second, adjust as needed
+  });
+}
